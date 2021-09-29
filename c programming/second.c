3709e517d00567cf06413e6b0340d6f4d4103b41
stella08312 <20211915@edu.hanbat.ac.kr>
2021-09-29 15:59:32 +0000

    c programming

---
+++ c programming/second.c
@@ -0,0 +1,18 @@
+//백준 1008번 문제//
+
+#include <stdio.h>
+
+int main()
+{
+  float A,B;
+
+  printf("정수 A를 입력하시오:\n ");
+  scanf("%f", &A);
+
+  printf("정수 B를 입력하시오:\n");
+  scanf("%f", &B);
+
+  printf("A/B = %f", A/B);
+
+  return 0;
+}
